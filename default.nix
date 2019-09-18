dae6b17463f27e5520f23da1ec596c2c15f18c97
Tim Nelson <tim@eltimn.com>
2019-09-18 14:30:58 -0500

    Upgraded bower

---
+++ default.nix
@@ -0,0 +1,11 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "env";
+  env = buildEnv { name = name; paths = buildInputs; };
+  buildInputs = [
+    openjdk
+    sbt
+    nodejs-10_x
+    yarn
+  ];
+}
